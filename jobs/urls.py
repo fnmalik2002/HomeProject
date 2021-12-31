0c15199ca8b6f2df163d11b0b16b1690451aaf6b
Faizan Malik <65131378+fnmalik2002@users.noreply.github.com>
2021-12-30 22:47:07 -0600

    Dashboard for each user is made but it is not perfect yet

--- jobs/urls.py
+++ jobs/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('mark_done/', views.mark_done, name='mark_done'),
     path('review_done/', views.review_done, name='review_done'),
     path('mark_not_done/', views.mark_done, name='mark_not_done'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.http import HttpResponseRedirect, HttpResponse
 # from django.http.request import HttpRequest
 from django.template import loader
@@ -9,11 +10,89 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
 from django.utils import timezone
 from django.db.models import Avg, Count, Min, Sum
+import matplotlib.pyplot as plt
+from io import StringIO
+import numpy as np
 
 
 my_money_under_review_this_month = None
 my_total_this_mont = None
 unclaimed = None
+
+def get_data(request):
+    print("user type : ",type(request.user))
+    user = User.objects.get(username = request.user)
+    user_group = Group.objects.get(user = request.user)
+    print('user_group \n', user_group)
+    
+    users_in_a_group = User.objects.filter(groups=user_group)
+    print('users in group \n',users_in_a_group )
+    def job_creators(users_in_a_group):
+        poster = None
+        for u in users_in_a_group:
+            job_list_1 = JobPost.objects.filter(job_creator = u).order_by('-id')[:10] 
+            print (u, job_list_1, len(job_list_1))
+            if len(job_list_1) > 0:
+                print("job creator is", u.id)
+                poster = u
+                print(u)
+                
+            else:
+                print('None')
+
+        return poster
+    date_today = timezone.datetime.now()            
+    # print('current month :' , timezone.datetime.now().month)
+    # print("Jobs on a specific date : ", JobPost.objects.filter(publish_date__month=date_today.month))
+    this_family_jobs = JobPost.objects.filter(publish_date__month=date_today.month).filter(job_creator=job_creators(users_in_a_group))
+
+    print('this_family_jobs : ', this_family_jobs)
+    
+    if request.user.is_staff:
+        print("I am a staff")
+        my_jobs_under_review = this_family_jobs.filter(job_taker=user).filter(job_taken= True).filter(job_under_review= True).order_by('-id')[:10] 
+        my_jobs_acceptably_done = this_family_jobs.filter(job_taker=user).filter(job_taken=True).filter(job_done=True).filter(job_passed_review= True).order_by('-id')[:10]
+        
+        jobs_under_review = this_family_jobs.filter(job_taken= True).filter(job_under_review= True).order_by('-id')[:10] 
+        jobs_acceptably_done = this_family_jobs.filter(job_taken=True).filter(job_done=True).filter(job_passed_review= True).order_by('-id')[:10]
+        
+        job_list = this_family_jobs.filter(job_taken= False).order_by('-id')[:10] 
+        
+        job_gone = this_family_jobs.filter(job_taken= True).order_by('-id')[:10]
+        my_unfinished_jobs = this_family_jobs.filter(job_taker=user).filter(job_taken=True).filter(job_done=False).order_by('-id')[:10]
+        all_unfinished_jobs = this_family_jobs.filter(job_taken=True).filter(job_done=False).order_by('-id')[:10]
+
+        my_done_jobs = this_family_jobs.filter(job_taker=user).filter(job_done=True).order_by('-id')[:10]
+        all_done_jobs = this_family_jobs.filter(job_taken=True).filter(job_done=True).order_by('-id')[:10]
+        my_money_under_review_this_month = this_family_jobs.filter(job_taken=True).filter(job_under_review= True).filter(job_done=True).aggregate(Sum('job_price'))
+        my_total_this_mont = this_family_jobs.filter(job_taken=True).filter(job_done=True).filter(job_passed_review= True).aggregate(Sum('job_price'))
+        unclaimed = this_family_jobs.filter(job_taken= False).aggregate(Sum('job_price'))
+        return my_jobs_under_review, my_jobs_acceptably_done, jobs_under_review, jobs_acceptably_done, job_list, job_gone, my_unfinished_jobs, all_unfinished_jobs, my_done_jobs, all_done_jobs, my_money_under_review_this_month, my_total_this_mont, unclaimed
+
+    else:
+        print("I am not a staff")
+        my_jobs_under_review = this_family_jobs.filter(job_taker=user).filter(job_taken= True).filter(job_under_review= True).order_by('-id')[:10] 
+        my_jobs_acceptably_done = this_family_jobs.filter(job_taker=user).filter(job_taken=True).filter(job_done=True).filter(job_passed_review= True).order_by('-id')[:10]
+        jobs_under_review = this_family_jobs.filter(job_taken= True).filter(job_under_review= True).order_by('-id')[:10] 
+        jobs_acceptably_done = this_family_jobs.filter(job_taken=True).filter(job_done=True).filter(job_passed_review= True).order_by('-id')[:10]
+        
+        
+        job_list = this_family_jobs.filter(job_taken= False).order_by('-id')[:10]  
+        
+        
+        job_gone = this_family_jobs.filter(job_taken= True).order_by('-id')[:10]
+        my_unfinished_jobs = this_family_jobs.filter(job_taker=user).filter(job_taken=True).filter(job_done=False).order_by('-id')[:10]
+        all_unfinished_jobs = this_family_jobs.filter(job_taken=True).filter(job_done=False).order_by('-id')[:10]
+        my_done_jobs = this_family_jobs.filter(job_taker=user).filter(job_done=True).order_by('-id')[:10]
+        all_done_jobs = this_family_jobs.filter(job_taken=True).filter(job_done=True).order_by('-id')[:10]
+        
+        my_money_under_review_this_month = this_family_jobs.filter(job_taker=user).filter(job_taken=True).filter(job_under_review= True).filter(job_done=True).aggregate(Sum('job_price'))
+        my_total_this_mont = this_family_jobs.filter(job_taker=user).filter(job_taken=True).filter(job_done=True).filter(job_passed_review= True).aggregate(Sum('job_price'))
+        unclaimed = this_family_jobs.filter(job_taken= False).aggregate(Sum('job_price'))
+        print(my_jobs_under_review, my_jobs_acceptably_done, jobs_under_review, jobs_acceptably_done, job_list, job_gone, my_unfinished_jobs, all_unfinished_jobs, my_done_jobs, all_done_jobs, my_money_under_review_this_month, my_total_this_mont, unclaimed
+        )
+        return my_jobs_under_review, my_jobs_acceptably_done, jobs_under_review, jobs_acceptably_done, job_list, job_gone, my_unfinished_jobs, all_unfinished_jobs, my_done_jobs, all_done_jobs, {'my_money_under_review_this_month' : my_money_under_review_this_month}, {'my_total_this_mont':my_total_this_mont}, {'unclaimed':unclaimed}
+
 
 # Create your views here.
 @login_required
@@ -39,10 +118,10 @@ def index(request):
                 print('None')
 
         return poster
-                
-
-    
-    this_family_jobs = JobPost.objects.filter(job_creator=job_creators(users_in_a_group))
+    date_today = timezone.datetime.now()            
+    # print('current month :' , timezone.datetime.now().month)
+    # print("Jobs on a specific date : ", JobPost.objects.filter(publish_date__month=date_today.month))
+    this_family_jobs = JobPost.objects.filter(publish_date__month=date_today.month).filter(job_creator=job_creators(users_in_a_group))
 
     print('this_family_jobs : ', this_family_jobs)
     
@@ -55,6 +134,7 @@ def index(request):
         jobs_acceptably_done = this_family_jobs.filter(job_taken=True).filter(job_done=True).filter(job_passed_review= True).order_by('-id')[:10]
         
         job_list = this_family_jobs.filter(job_taken= False).order_by('-id')[:10] 
+        
         job_gone = this_family_jobs.filter(job_taken= True).order_by('-id')[:10]
         my_unfinished_jobs = this_family_jobs.filter(job_taker=user).filter(job_taken=True).filter(job_done=False).order_by('-id')[:10]
         all_unfinished_jobs = this_family_jobs.filter(job_taken=True).filter(job_done=False).order_by('-id')[:10]
@@ -72,8 +152,10 @@ def index(request):
         jobs_under_review = this_family_jobs.filter(job_taken= True).filter(job_under_review= True).order_by('-id')[:10] 
         jobs_acceptably_done = this_family_jobs.filter(job_taken=True).filter(job_done=True).filter(job_passed_review= True).order_by('-id')[:10]
         
-
+        print (this_family_jobs.filter())
         job_list = this_family_jobs.filter(job_taken= False).order_by('-id')[:10]  
+        # print("publish_date : ", job_list)
+        
         job_gone = this_family_jobs.filter(job_taken= True).order_by('-id')[:10]
         my_unfinished_jobs = this_family_jobs.filter(job_taker=user).filter(job_taken=True).filter(job_done=False).order_by('-id')[:10]
         all_unfinished_jobs = this_family_jobs.filter(job_taken=True).filter(job_done=False).order_by('-id')[:10]
@@ -234,5 +316,17 @@ def mark_not_done(request):
     print("Not done selected")
     return index(request)
 
+@login_required
+def dashboard(request):
+
+
+    template = loader.get_template('jobs/dashboard.html')
+    context = { 
+    'user': request.user,
+    'data' : get_data(request)
+
+     }
+    return HttpResponse(template.render(context, request))
+    
 
     
